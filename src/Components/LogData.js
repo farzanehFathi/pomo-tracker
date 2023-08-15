8e3fe3a74873e363deeecc89ba56314d55bb3269
farzaneh <farzaneh.fth@gmail.com>
2023-08-15 06:47:17 +0330

    worked on logData component

--- src/Components/LogData.js
+++ src/Components/LogData.js
@@ -1,10 +1,30 @@
-import "./Components/LogData.css";
+import { useState } from "react";
+import "./LogData.css";
 
 export default function LogData() {
+  const [time, setTime] = useState(null);
+  const [item, setItem] = useState(null);
+
+  function handleChange(event) {
+    console.log(event);
+    // setTime(event.target().value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleChange();
+    setItem("Apple");
+  };
+
   return (
     <div className="LogData">
-      <form>
-        <input type="datetime-local" name="date-time" id="date_time_id" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="datetime-local"
+          name="date-time"
+          id="date_time_id"
+          onChange={handleChange}
+        />
         <input type="text" name="" id="" />
         <input type="number" name="" id="" />
         <input type="text" name="Description" id="" />
@@ -13,6 +33,9 @@ export default function LogData() {
 
       <div className="log-table">
         <div className="log-table-row">
+          <div className="log-table-col">{time}</div>
+          <div className="log-table-col">{item}</div>
+          <div className="log-table-col">5</div>
           <div className="log-table-col">5</div>
         </div>
       </div>
